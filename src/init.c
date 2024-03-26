2dbf11911773086519e99528da600bbc507936b6
El Houssain Souhail <sou.ductive@gmail.com>
2024-03-26 21:48:30 +0000

    Updated the initialization function

--- src/init.c
+++ src/init.c
@@ -1,22 +1,38 @@
 #include "maze.h"
 
-int init(SDL_Instance *instance)
+/**
+ * init_instance - Initializes an SDL window and renderer.
+ *
+ * @instance: pointer to the SDL window and renderer.
+ *
+ * Return: (true) if initialization is successful, (false) otherwise.
+*/
+int init_instance(SDL_Instance *instance)
 {
-	if(SDL_Init(SDL_INIT_VIDEO) < 0)
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
 	{
-		fprintf(stderr, "Unable to initialize SDL: %s\n", SDL_GetError());
-		return (1);
+		fprintf(stderr, "%s\n", SDL_GetError());
+		return (false);
+	}
+	/* Window Initialization */
+	instance->window = SDL_CreateWindow(
+		"C ft. SDL",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		WINDOW_WIDTH, WINDOW_HEIGHT,
+		0
+	);
+	if (!instance->window)
+	{
+		fprintf(stderr, "%s\n", SDL_GetError());
+		return (false);
+	}
+	/* Renderer Initialization */
+	instance->renderer = SDL_CreateRenderer(instance->window, -1, 0);
+	if (!instance->renderer)
+	{
+		fprintf(stderr, "%s\n", SDL_GetError());
+		return (false);
 	}
 
-    instance->window = SDL_CreateWindow("The Maze", SDL_WINDOWPOS_CENTERED,
-            SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
-    if (instance->window == NULL)
-    {
-        fprintf(stderr, "Unable to Create Window: %s\n", SDL_GetError());
-        SDL_Quit();
-        return (1);
-    }
-
-    return (0);
+	return (true);
 }
-
