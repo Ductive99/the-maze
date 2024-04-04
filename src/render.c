70eb7f4366e82df8b973e30de988a0bd0cdcdb08
El Houssain Souhail <sou.ductive@gmail.com>
2024-04-04 21:28:50 +0000

    Added ray rendering functions

--- src/render.c
+++ src/render.c
@@ -16,24 +16,13 @@ const int map[ROWS][COLS] = {
 	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 };
 
-/**
- * scaler - multiplies a value by a scale factor
- *
- * @value: value to multiply
- *
- * Return: the product
-*/
-float scaler(float value)
-{
-	return (value * MAP_SCALE);
-}
+Ray rays[RAYS];
 
 /**
  * render - renders the frame.
  *
- * @instance: pointer to the window and renderer.
- * @player: pointer to the player struct.
- * @map: the map's 2D array.
+ * @instance: Pointer to the window and renderer.
+ * @player: Pointer to the player struct.
 */
 void render(SDL_Instance *instance, Player *player)
 {
@@ -41,6 +30,7 @@ void render(SDL_Instance *instance, Player *player)
 	SDL_RenderClear(instance->renderer);
 
 	renderMap(instance);
+	renderRays(instance, player);
 	renderPlayer(instance, player);
 
 	SDL_RenderPresent(instance->renderer);
@@ -49,8 +39,8 @@ void render(SDL_Instance *instance, Player *player)
 /**
  * renderPlayer - renders a player.
  *
- * @instance: pointer to the window and renderer.
- * @player: pointer to the player struct.
+ * @instance: Pointer to the window and renderer.
+ * @player: Pointer to the player struct.
 */
 void renderPlayer(SDL_Instance *instance, Player *player)
 {
@@ -75,8 +65,7 @@ void renderPlayer(SDL_Instance *instance, Player *player)
 /**
  * renderMap - renders the 2D map.
  *
- * @instance: pointer to the window and renderer.
- * @map: the map's 2D array.
+ * @instance: Pointer to the window and renderer.
 */
 void renderMap(SDL_Instance *instance)
 {
@@ -104,13 +93,160 @@ void renderMap(SDL_Instance *instance)
 	}
 }
 
-bool isBlocked(float x, float y)
+
+void renderRays(SDL_Instance *instance, Player *player)
 {
-	if (x < 0 || x > WINDOW_WIDTH || y < 0 || y > WINDOW_HEIGHT)
-		return true;
+    SDL_SetRenderDrawColor(instance->renderer, 255, 0, 0, 255);
+    for (int i = 0; i < RAYS; i++) {
+        SDL_RenderDrawLine(
+            instance->renderer,
+            scaler(player->x),
+            scaler(player->y),
+            scaler(rays[i].wallHitX),
+            scaler(rays[i].wallHitY)
+        );
+    }
+}
 
-	int indexX = floor(x / TILE_SIZE);
-	int indexY = floor(y / TILE_SIZE);
+/**
+ * castRays - Calls the castRay() function with the correct ray.
+ *
+ * @player: Pointer to the player struct.
+*/
+void castRays(Player *player)
+{
+	float rayAngle = player->rotAngle - (FOV / 2);
 
-	return map[indexY][indexX] != 0;
+	for (int i = 0; i < RAYS; i++)
+	{
+		castRay(player, normalize(rayAngle), i);
+		rayAngle += FOV / RAYS;
+	}
+}
+
+/**
+ * castRay - Casts a single ray until encountring a wall.
+ *
+ * @player: Pointer to the player struct.
+ * @rayAngle: Angle relative to the player's orientation.
+ * @Id: The id of the ray.
+*/
+void castRay(Player *player, float rayAngle, int Id)
+{
+	/* 1 for down  -  0 for up */
+	int UPorDOWN = rayAngle > 0 && rayAngle < PI;
+	/* 1 for right - 0 for left */
+	int RIGHTorLEFT = rayAngle < 0.5 * PI || rayAngle > 1.5 * PI;
+
+	float xintercept, yintercept;
+	float xstep, ystep;
+
+	/* HORIZONTAL RAY-GRID INTERSECTION */
+	bool horzWallIsHit = false;
+	float horzWallHitX = 0;
+	float horzWallHitY = 0;
+	float horzWallContent = 0;
+
+	yintercept = floor(player->y / TILE_SIZE) * TILE_SIZE;
+	yintercept += UPorDOWN ? TILE_SIZE : 0;
+
+	xintercept = player->x + (yintercept - player->y) / tan(rayAngle);
+
+	ystep = !UPorDOWN ? -TILE_SIZE : TILE_SIZE;
+
+	xstep = TILE_SIZE / tan(rayAngle);
+	xstep *= (!RIGHTorLEFT && xstep > 0) ? -1 : 1;
+	xstep *= (RIGHTorLEFT && xstep < 0) ? -1 : 1;
+
+	float nextHtouchX = xintercept;
+	float nextHtouchY = yintercept;
+
+	while ((nextHtouchX >= 0 && nextHtouchX <= WINDOW_WIDTH) && 
+		(nextHtouchY >= 0 && nextHtouchY <= WINDOW_HEIGHT))
+	{
+		float xCheck = nextHtouchX;
+		float yCheck = nextHtouchY + (UPorDOWN ? 0 : -1);
+
+		if (isBlocked(xCheck, yCheck))
+		{
+			horzWallHitX = nextHtouchX;
+			horzWallHitY = nextHtouchY;
+            horzWallContent = map[(int)floor(yCheck / TILE_SIZE)][(int)floor(xCheck / TILE_SIZE)];
+			horzWallIsHit = true;
+			break;
+		}
+		else
+		{
+			nextHtouchX += xstep;
+			nextHtouchY += ystep;
+		}
+	}
+
+	/* VERTICAL RAY-GRID INTERSECTION */
+	bool vertWallIsHit = false;
+	float vertWallHitX = 0;
+	float vertWallHitY = 0;
+	int vertWallContent = 0;
+
+	xintercept = floor(player->x / TILE_SIZE) * TILE_SIZE;
+	xintercept += RIGHTorLEFT ? TILE_SIZE : 0;
+
+	yintercept = player->y + (xintercept - player->x) * tan(rayAngle);
+
+	xstep = RIGHTorLEFT ? TILE_SIZE : -TILE_SIZE;
+
+	ystep = TILE_SIZE * tan(rayAngle);
+	ystep *= (!UPorDOWN && ystep > 0) ? -1 : 1;
+	ystep *= (UPorDOWN && ystep < 0) ? -1 : 1;
+
+	float nextVtouchX = xintercept;
+	float nextVtouchY = yintercept;
+
+	while ((nextVtouchX >= 0 && nextVtouchX <= WINDOW_WIDTH) &&
+		(nextVtouchY >= 0 && nextVtouchY <= WINDOW_HEIGHT))
+	{
+		float xCheck = nextVtouchX - (RIGHTorLEFT ? 0 : 1);
+		float yCheck = nextVtouchY;
+
+		if (isBlocked(xCheck, yCheck))
+		{
+			vertWallHitX = nextVtouchX;
+			vertWallHitY = nextVtouchY;
+			vertWallContent = map[(int)floor(yCheck / TILE_SIZE)][(int)floor(xCheck / TILE_SIZE)];
+			vertWallIsHit = true;
+			break;
+		}
+		else
+		{
+			nextVtouchX += xstep;
+			nextVtouchY += ystep;
+		}
+	}
+
+	float horzHitDistance = horzWallIsHit
+		? distanceBtwn(player->x, player->y, horzWallHitX, horzWallHitY)
+		: FLT_MAX;
+	float vertHitDistance = vertWallIsHit
+		? distanceBtwn(player->x, player->y, vertWallHitX, vertWallHitY)
+		: FLT_MAX;
+
+	if (vertHitDistance < horzHitDistance)
+	{
+        rays[Id].distance = vertHitDistance;
+        rays[Id].wallHitX = vertWallHitX;
+        rays[Id].wallHitY = vertWallHitY;
+        rays[Id].wallHitContent = vertWallContent;
+        rays[Id].wasHitVertical = 1;
+	}
+	else
+	{
+        rays[Id].distance = horzHitDistance;
+        rays[Id].wallHitX = horzWallHitX;
+        rays[Id].wallHitY = horzWallHitY;
+        rays[Id].wallHitContent = horzWallContent;
+        rays[Id].wasHitVertical = 0;
+	}
+    rays[Id].rayAngle = rayAngle;
+    rays[Id].UorD = UPorDOWN;
+    rays[Id].RorL = RIGHTorLEFT;
 }
